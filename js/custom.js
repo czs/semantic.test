03c50b7ac65ddc10b8f50c881d8b3c9260062a18
weichx <Xi.Weicheng@emc.com>
2014-09-30 11:29:03 +0800

    [unfixed-bug] if modal height > body height, body will be set height, override previous height

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,44 @@
 jQuery(document).ready(function($) {
 
-			$('#btnSiderBar').click(function(event) {
-						$('#siderLeftBar').sidebar('toggle');
-					});
+	$('#btnSiderBar').click(function(event) {
+		$('#siderLeftBar').sidebar('toggle');
+	});
 
-			$('#siderLeftBar').sidebar({
-					// overlay : true
-					}).sidebar('show');
+	$('#siderLeftBar').sidebar({
+	// overlay : true
+	}).sidebar('show');
 
-			$(window).resize(function() {
-						$('body').css('height', '100%');
-					});
+	$(window).resize(function() {
+		$('body').css('height', '100%');
+	});
 
-			$('.ui.dropdown').dropdown();
+	$('.ui.dropdown').dropdown();
 
-			$('.ui.modal').modal();
+	$('.ui.modal').modal({
+		closable : false,
+		onHidden : function() {
+			$('body').css('height', '100%');
+		}
+	});
 
-			$('#btnAdd').click(function() {
-						$('#modalAdd').modal('show');
-					});
+	$('#btnAdd').click(function() {
+		$('#modalAdd').modal('show');
+	});
 
-			$('#btnUpdate').click(function() {
-						$('#modalUpdate').modal('show');
-					});
+	$('#btnUpdate').click(function() {
+		$('#modalUpdate').modal('show');
+	});
 
-			$('#btnDelete').click(function() {
-						$('#modalDelete').modal('show');
-					});
+	$('#btnDelete').click(function() {
+		$('#modalDelete').modal('show');
+	});
 
-			$('table').tablesort();
-			$('thead th.number').data('sortBy', function(th, td, sorter) {
-						if (!!$(td).attr('data-sort-value')) {
-							return parseInt($(td).attr('data-sort-value'), 10);
-						}
-						return parseInt(td.text(), 10);
-					});
+	$('table').tablesort();
+	$('thead th.number').data('sortBy', function(th, td, sorter) {
+		if (!!$(td).attr('data-sort-value')) {
+			return parseInt($(td).attr('data-sort-value'), 10);
+		}
+		return parseInt(td.text(), 10);
+	});
 
-		});
+});
